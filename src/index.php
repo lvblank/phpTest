8695b2d1af6a527e6b27744dbed9fd2b727ff183
lvblank <2287845704@qq.com>
2016-09-17 11:56:39 +0800

    first add file

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+	//$fileStr = "test.php";
+	$fileStr = "PDOFunc.php";
+	include($fileStr);
+	
+	phpinfo();
+	$server = 'localhost';
+	$user = 'root';
+	$password = 'root';
+	$table = '';
+	/*
+	$link = mysql_connect($server, $user, $password) or die('cannot connect!');
+	echo "success!";
+	mysql_close($link);
+	*/
+?>
